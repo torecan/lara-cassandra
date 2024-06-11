36f827bb2d4b1018c11e39c8ffef8ccb21660a11
michael.roosz <michael.roosz@check24.de>
2024-06-11 22:59:20 +0200

    Fix "allowFiltering()", add basic support for pagination

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace LaraCassandra\Query;
 
+use Closure;
 use InvalidArgumentException;
 use Illuminate\Database\Query\Builder as BaseBuilder;
-
+use Illuminate\Pagination\Paginator;
 use LaraCassandra\Connection;
 use LaraCassandra\Consistency;
 use RuntimeException;
 
 class Builder extends BaseBuilder {
+    public bool $allowFiltering = false;
     /**
      * The current query value bindings.
      *
@@ -47,6 +49,34 @@ class Builder extends BaseBuilder {
         return $this;
     }
 
+    /**
+     * Allow filtering for this query
+     *
+     * @param  bool  $value
+     * @return $this
+     */
+    public function allowFiltering(bool $value = true) {
+        $this->allowFiltering = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get a paginator only supporting simple next and previous links.
+     *
+     * This is more efficient on larger data-sets, etc.
+     *
+     * @param  int|null  $perPage
+     * @param  array<string>|string  $columns
+     * @param  string  $cursorName
+     * @param  \Illuminate\Pagination\Cursor|string|null  $cursor
+     * @return \Illuminate\Contracts\Pagination\CursorPaginator
+     */
+    public function cursorPaginate($perPage = 15, $columns = ['*'], $cursorName = 'cursor', $cursor = null) {
+
+        throw new RuntimeException('Cursor pagination is not supported by Cassandra');
+    }
+
     public function delete($id = null) {
         $this->applyConsistency();
 
@@ -72,10 +102,55 @@ class Builder extends BaseBuilder {
         return $this;
     }
 
+    /**
+     * Paginate the given query into a simple paginator.
+     *
+     * @param  int|\Closure  $perPage
+     * @param  array<string>|string  $columns
+     * @param  string  $pageName
+     * @param  int|null  $page
+     * @param  \Closure|int|null  $total
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function paginate($perPage = 15, $columns = ['*'], $pageName = 'page', $page = null, $total = null) {
+        $page = $page ?: Paginator::resolveCurrentPage($pageName);
+
+        $total = value($total) ?? $this->getCountForPagination();
+        if (!is_numeric($total)) {
+            throw new InvalidArgumentException('Argument $total must be numeric');
+        }
+
+        $total = (int) $total;
+
+        $perPage = $perPage instanceof Closure ? $perPage($total) : $perPage;
+
+        $results = $total ? $this->get($columns) : collect();
+
+        return $this->paginator($results, $total, $perPage, $page, [
+            'path' => Paginator::resolveCurrentPath(),
+            'pageName' => $pageName,
+        ]);
+    }
+
     public function setConsistency(Consistency $level): self {
         $this->consistency = $level;
 
         return $this;
+    }
+
+    /**
+     * Get a paginator only supporting simple next and previous links.
+     *
+     * This is more efficient on larger data-sets, etc.
+     *
+     * @param  int  $perPage
+     * @param  array<string>|string  $columns
+     * @param  string  $pageName
+     * @param  int|null  $page
+     * @return \Illuminate\Contracts\Pagination\Paginator
+     */
+    public function simplePaginate($perPage = 15, $columns = ['*'], $pageName = 'page', $page = null) {
+        throw new RuntimeException('Simple pagination is not supported by Cassandra.');
     }
 
     /**
